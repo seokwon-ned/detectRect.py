4279d2330847a444b21ab1e482e68bd772af78e8
seokwon-ned <seokwon.nedsys@gmail.com>
2021-07-11 22:05:35 +0900

    1st commit

---
+++ detectRect.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Load iamge, grayscale, adaptive threshold
+image = cv2.imread('1.png')
+result = image.copy()
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,2)
+
+# Fill rectangular contours
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    cv2.drawContours(thresh, [c], -1, (255,255,255), -1)
+
+# Morph open
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
+
+# Draw rectangles
+cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 1)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('opening', opening)
+cv2.imshow('image', image)
+cv2.waitKey()
